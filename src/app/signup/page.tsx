1686501fba6b872a0038114e41fca5aa768b7691
MetriSomesh <someshmetri44@gmail.com>
2024-11-13 12:59:11 +0530

    deployment err fixes

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,7 +3,7 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { signIn } from "next-auth/react";
+// import { signIn } from "next-auth/react";
 import Link from "next/link";
 import { useState } from "react";
 // import { FcGoogle } from "react-icons/fc";
